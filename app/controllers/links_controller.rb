3ce65f89e02639a5aa7d2d9cc666f276f841cd92
Corey Speisman <Cspeisman@gmail.com>
2014-01-11 18:52:35 -0500

    adding new links to friendships

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,16 @@
 class LinksController < ApplicationController
 	def new
+		@friendship = Friendship.find(params[:friendship_id])
+		@link = Link.new
 	end
 	
 	def create
+	  @friendship = Friendship.find(params[:friendship_id])
+	  @link = @friendship.links.build(params[:link].permit(:url))
+	  if @link.save
+	  	redirect_to friendship_path(@friendship)
+		else
+			render 'new'
+		end
 	end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,8 @@
 class Link < ActiveRecord::Base
 	belongs_to :friendship
 	validates :friendship_id, presence: true
+	validates :url, format: { with: /https?:\/\/www.[\S]+/,
+    message: "This does not appear to be a valid URL" }
 
 	def self.link_allowed?(user_id, friend_id)
 		friendship = Friendship.where(user_id: user_id, friend_id: friend_id).first
